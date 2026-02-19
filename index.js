e4fd8b7eaf60323a2f76c72eaf897da863785644
scarls00 <seritarak@gmail.com>
2026-02-18 19:18:12 -0600

    update styling and layout

---
+++ index.js
@@ -0,0 +1,13 @@
+function contact(event) {
+    event.preventDefault
+    emailjs
+        .sendForm(
+            'service_5mjnnki',
+            'template_ssolw39',
+            event.target,
+            'lgVtYYk8QAylAMCav'
+        ).then(() => {
+            console.log('it worked')
+        })
+    }
+
