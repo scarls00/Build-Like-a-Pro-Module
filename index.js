7a85e77260638d3bb1a3f32782896cc84cfa4aaf
scarls00 <seritarak@gmail.com>
2026-02-22 13:27:32 -0600

    update styling and layout

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
+let isModalOpen = false;
+let contrastToggle = false;
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+    document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme");
+    }
+
+}
+
 function contact(event) {
     event.preventDefault()
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classlist += " modal__overlay--visible"
     emailjs
         .sendForm(
             'service_5mjnnki',
             'template_ssolw39',
             event.target,
             'lgVtYYk8QAylAMCav'
-        ).then(() => {
-            console.log('it worked')
-        })
+    ).then(() => {
+            loading.classList.remove("modal__overlay--visible");
+            success.classlist += " modal__overlay--visible";
+    }).catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+            "Email service temporarily unavailable. Please message me directly at: ReyAdlerDesigns@gmail.com."
+            );
+    })
+}
+
+function toggleModal() {
+    if (isModalOpen) {
+        return document.body.classList.remove("modal--open");
     }
+    isModalOpen = !isModalOpen;
+    document.body.classList += " modal--open";
+}
 
